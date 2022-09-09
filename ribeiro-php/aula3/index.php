e225ea437eed13560357c266b5df14d07d02a60a
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-09 20:28:15 +0900

    Create index.php

---
+++ ribeiro-php/aula3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+</html>
+
+<?php
+  $variavel = 100;
+  $variavel = 'Teste';
+  $variavel = 'Uma variavel';
+  $Variavel = 'Outra variavel';
+
+  $var1 = 0;
+  
+  echo "$variavel e batata e $Variavel <br>";
+  echo "$var1";
+
+  $umaVariavel = 0;
+  $uma_variavel = 0; 
+
+
+?>
