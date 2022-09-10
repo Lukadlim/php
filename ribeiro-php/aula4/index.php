87ae9b7814cb406201be2181ab7a6aaacca39537
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-10 22:33:17 +0900

    Create index.php

---
+++ ribeiro-php/aula4/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $var = 100;
+    $var = 'cem';
+    $var = 10.5;
+    $var = true;
+    $var = [1, 2, 3];
+    // $var = new Pessoa();
+    $var = null;
+
+    // print_r($var[0]);
+    echo "$var";
+    
+
+?>
