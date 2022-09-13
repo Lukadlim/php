ab8887ea602d5be5056622d731be067b6a193e61
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-13 19:25:32 +0900

    Create index.php

---
+++ ribeiro-php/aula11/index.php
@@ -0,0 +1,40 @@
+<?php
+    // echo '<p>Este é um parágrafo de HTML</p>';
+    // echo '<br>';
+
+    // $frase1 =  'i'm going to John's Party!'; 
+                   #jeito errado
+
+    // $frase2 = "I'm going to John's Party! <br>";
+
+    // $frase3 = "i'm not sure John "The great"  will win this fight"; 
+                   #jeito errado
+
+    // echo $frase2;
+
+    // $frase1 = 'I\'m going to John\'s Party! <br>';
+
+    // echo $frase1;
+
+    // $frase3 = "I'm not sure John \"The great\"  will win this fight";
+    // $frase3 = 'i\'m not sure John "The great"  will win this fight'; 
+
+    // echo $frase3;
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    // echo 'João Ribeiro\n\rTeste';
+    // echo "João Ribeiro Teste";
+    // echo "\rMary";
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    echo "\u{a9} ";
+    echo "\u{bc} ";
+    echo "\u{AE} ";
+
+    echo "<hr>";
+    echo "&copy ";
+    echo "&frac14 ";
+    echo "&reg";
+?>
