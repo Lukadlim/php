c378c35518c7fd2e7ae3430717b9d4e4a22575fe
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-15 20:41:06 +0900

    Create index.php

---
+++ ribeiro-php/aula17/index.php
@@ -0,0 +1,49 @@
+<?php
+    // $nome = 'joao';
+
+    // switch ($nome) {
+    //     case 'ana':
+    //         echo 'Seu nome é ana';
+    //         break;
+    //     case 'joao':
+    //         echo 'red';
+    //         break;
+    //     default:
+    //         echo 'Seu nome não está listado';
+    //     break;
+    // }
+
+    ////////////////////////////////////////////////////////////
+
+    $valor = 20;
+
+    // if ($valor == 10):
+    //     echo 'O valor é 10';
+    // elseif ($valor == 5):
+    //     echo 'O valor é 5';
+    // else:
+    //     echo "O valor não é 10 é $valor";
+    // endif;
+
+    ////////////////////////////////////////////////////////////
+
+    // switch ($valor):
+    //     case (10):
+    //         echo 'O valor é 10 mesmo';
+    //         break;
+    //     case (20):
+    //         echo 'O valor é 20';
+    //         break;
+    //     default:
+    //         echo "O valor não é 10 é $valor mesmo";
+    //     endswitch;
+
+    ////////////////////////////////////////////////////////////
+?>
+
+<?php// if ($valor == 20): ?>
+    <!-- <h1>Olá mundo</h1> -->
+<?php //else: ?>
+    <!-- <h2>Não olá mundo</h2> -->
+<?php //endif; ?>
+
