2bc631e1abe9606d8431be2fe617e075d4ba8b50
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-15 19:24:11 +0900

    Create index.php

---
+++ ribeiro-php/aula16/index.php
@@ -0,0 +1,89 @@
+<?php
+    $nome = 'joao';
+
+    if ($nome == 'joao') {
+        // echo 'Foi identificado o nome do autor do vídeo';
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    $idade = 17;
+
+    if ($idade < 18) {
+    //     echo 'Esta pessoa é adolescente';
+    // } else {
+    //     echo 'Esta pessoa é adulta';
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    // $nota = 2.1;
+
+    // if ($nota <= 2) {
+    //     echo 'Nota fraca';
+    // } elseif ($nota <= 4) {
+    //     echo 'Nota insuficiente';
+    // } elseif ($nota <= 6) {
+    //     echo 'Nota positiva';
+    // } elseif ($nota <= 8) {
+    //     echo 'Nota muito positiva';
+    // } else {
+    //     echo 'Nota execelente';
+    // }
+
+    //////////////////////////////////////////////////////////////////////
+
+    // $nota = 7;
+
+    // if ($nota <= 2)
+    //     echo 'Nota fraca';
+    // elseif ($nota <= 4)
+    //     echo 'Nota insuficiente';
+    // elseif ($nota <= 6)
+    //     echo 'Nota positiva';
+    // elseif ($nota <= 8)
+    //     echo 'Nota muito positiva';
+    // else
+    //     echo 'Nota execelente';
+        
+    //////////////////////////////////////////////////////////////////////
+
+    $nota = 2;
+
+    // if ($nota <= 2) {
+    //     echo 'Nota fraca <br>';
+    //     echo 'Reportar ao diretor da escola';
+    // }
+    // elseif ($nota <= 4)
+    //     echo 'Nota insuficiente';
+    // elseif ($nota <= 6)
+    //     echo 'Nota positiva';
+    // elseif ($nota <= 8)
+    //     echo 'Nota muito positiva';
+    // else
+    //     echo 'Nota execelente';
+
+    //////////////////////////////////////////////////////////////////////
+
+    // $n1 = 28;
+
+    // if ($n1 > 10 && $n1 != 30) {
+    //     echo "O numero $n1 é maior que 10 e difenerente de 30";
+    // } else {
+    //     echo 'Afirmação falsa';
+    // }
+
+    //////////////////////////////////////////////////////////////////////
+
+    $n1 = -100;
+
+    if ($n1 > 0) {
+        if ($n1 >= 100) {
+            echo 'O número é igual ou maior que 100';
+        } else {
+            echo 'O número é positivo, mas inferior a 100';
+        }
+    } else {
+        echo 'O número é negativo';
+    }
+?>
