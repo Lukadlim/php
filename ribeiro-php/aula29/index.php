b7eaaa48c557e378a6094d079c6b770ad1188004
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-19 13:52:12 +0900

    Create index.php

---
+++ ribeiro-php/aula29/index.php
@@ -0,0 +1,26 @@
+<?php
+    function outraFuncao($a) {
+        $x = func_get_arg(0);
+        $y = func_get_arg(1);
+        $z = func_get_arg(2);
+
+        echo "$x $y $z";
+
+        echo '<br>';
+
+        echo func_num_args();
+    };
+
+    outraFuncao(10, 5, 30);
+
+    echo '<hr>';
+
+    function minha_funcao(...$argumentos) {
+        foreach($argumentos as $v) {
+            echo "$v <br>";
+        }
+    }
+
+    minha_funcao(10, 20, 30, 40);
+?>
+
