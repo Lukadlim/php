4c6adf198a10a2c88d9976ebee76fa52e84c5a88
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-09-23 09:33:29 +0900

    Create index.php

---
+++ ribeiro-php/aula044/index.php
@@ -0,0 +1,22 @@
+<?php
+    class Humano
+    {
+        private $nome = 'x';
+
+        function setPrivate($objeto, $valor){
+            $objeto->nome = $valor;
+        }
+
+        function apresentar(){
+            echo $this->nome;
+        }
+    }
+
+    $a = new Humano();
+    $b = new Humano();
+    $a->setPrivate($b, 'João');
+
+    $a->apresentar();
+    echo '<br>';
+    $b->apresentar();
+?>
