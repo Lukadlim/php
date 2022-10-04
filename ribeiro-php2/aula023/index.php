d73fcb0ebf5601731f69dd707f35e5ecb5dfc44f
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-10-04 23:54:54 +0900

    Create index.php

---
+++ ribeiro-php2/aula023/index.php
@@ -0,0 +1,14 @@
+<?php
+    printf("O meu nome é %s e o apelido é %s", 'João', 'Ribeiro');
+    echo '<br>';
+
+    $valor = 65;
+
+    printf('%b', $valor);
+    echo '<br>';
+
+    printf('%c', $valor);
+    echo '<br>';
+
+    printf('%d', $valor);
+?>
