4d33899b40e9a9203ba6bf8a1f8da8da2a22ff92
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-10-10 14:03:01 +0900

    Create index.php

---
+++ ribeiro-php2/aula047/index.php
@@ -0,0 +1,22 @@
+<?php
+    $nomes = array('a', 'b', 'c',);
+    $nomes2 = ['a', 'b', 'c',];
+    $nomes3[30] = 'João';
+
+    print_r($nomes3);
+
+    $cliente = [
+        'nome' => 'Carlos',
+        'idade' => 35,
+        'profissao' => 'Administrador',
+    ];
+
+    print_r($cliente);
+
+    $cliente2['nome'] = 'João';
+    $cliente2['idade'] = 40;
+    $cliente2['profissao'] = 'Programador';
+
+    print_r($cliente2)
+
+?>
