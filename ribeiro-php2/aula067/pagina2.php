3be2b9acba7c1b820e99bfb42c1bdd6b392b7b31
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-10-14 00:07:57 +0900

    get

---
+++ ribeiro-php2/aula067/pagina2.php
@@ -0,0 +1,29 @@
+<?php
+    if(!isset($_GET['nome'])){
+        // exit();
+        // die();
+        header('Location: index.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome = htmlspecialchars($_GET['nome']);
+    ?>
+
+    <h3>Página 2</h3>
+
+    <p><?= $nome?></p>
+
+    <a href="index.php">Ir para a página 1</a>
+    
+</body>
+</html>
