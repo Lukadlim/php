12adcd2cce999217e051c765085efcbdb7c80a01
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-10-20 18:37:33 +0900

    Create index.php

---
+++ ribeiro-php2/aula085/index.php
@@ -0,0 +1,38 @@
+<style>
+    * {
+        font-family: Arial;
+    }
+</style>
+
+<?php
+    session_start();
+
+    echo '<pre>';
+
+    $_SESSION['nome'] = 'João';
+    $_SESSION['valor'] = 1000;
+    $_SESSION['frutas'] = ['Banana', 'Maçã', 'Melão'];
+    print_r($_SESSION);
+
+    echo '<hr>';
+
+    unset($_SESSION['valor']);
+    print_r($_SESSION);
+
+    echo '<hr>';
+
+    $_SESSION['nome'] = 'Ribeiro';
+    print_r($_SESSION);
+
+    echo '<hr>';
+
+    echo "Nome: " . $_SESSION['nome'];
+    echo '<br>';
+    echo 'Uma fruta: ' . $_SESSION['frutas'][1];
+    
+    echo '<hr>';
+
+    // session_destroy();
+    session_unset();
+    print_r($_SESSION);
+?>
