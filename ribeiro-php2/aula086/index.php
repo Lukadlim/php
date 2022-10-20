7131d8c81a62c44cf2612001a5595086ec4bd35c
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-10-20 22:06:37 +0900

    Create index.php

---
+++ ribeiro-php2/aula086/index.php
@@ -0,0 +1,8 @@
+<?php
+    $gestor = new PDO("mysql:host=localhost;dbname=loja", "root", "");
+    $dados = $gestor->query("SELECT * FROM clientes");
+    $clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<pre>';
+    print_r($clientes);
+?>
