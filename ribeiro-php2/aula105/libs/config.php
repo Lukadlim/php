248fda59490f0bd4f5535eedc58ab8010bb05456
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-11-12 09:27:12 +0900

    login system

---
+++ ribeiro-php2/aula105/libs/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('MYSQL_DATA', 'club');
+    define('MYSQL_USER', 'user_club');
+    define('MYSQL_PASS', '5eTehI1a6Ovo');
+
+    define('AES_KEY', 'aIZGX2oEDkvtwBs3HmE8lYO0GE');
+?>
