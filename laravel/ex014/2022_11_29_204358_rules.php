15ea5d0b7c4f570703516bfc59017390fb98ad25
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-11-30 05:56:09 +0900

    relations

---
+++ laravel/ex014/2022_11_29_204358_rules.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_user', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+
+            $table->unsignedBigInteger('id_rule');
+            $table->foreign('id_rule')->references('id')->on('rules');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rules');
+        Schema::dropIfExists('rule_user');
+    }
+};
