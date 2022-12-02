d19ee98d3d363f907ddbdfaaa442adb5138ce88d
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-12-03 07:56:57 +0900

    products list

---
+++ laravel/ex023/home.blade.php
@@ -0,0 +1,29 @@
+@extends('site.layout')
+@section('title', 'This is our home page')
+
+@section('content')
+
+    <div class="row container">
+
+      @foreach ($products as $product)
+
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                  <img src=" {{ $product->image }} ">
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+                </div>
+                <div class="card-content">
+                  <span class="card-title"> {{ $product->name }} </span>
+                  <p>{{ Str::limit($product->description, 20) }}</p>
+                </div>
+              </div>
+        </div>
+
+      @endforeach
+
+    </div>
+
+@endsection
+
+
