b497a65e638313d8903ca577173429a9f4044a1d
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-12-07 04:11:18 +0900

    belongsTo

---
+++ laravel/ex026/details.blade.php
@@ -0,0 +1,22 @@
+@extends('site.layout')
+@section('title', 'This is our home page')
+
+@section('content')
+
+    <div class="row container">
+        <div class="col s12 m6">
+            <img src="{{ $product->image }}" class="responsive-img">
+        </div>
+        
+        <div class="col s12 m6">
+            <p style="font-size: 2em"> {{ $product->name }} </p>
+            <p> {{ $product->description }} </p>
+            <p>
+                Posted by: {{ $product->user->firstname }} <br>
+                Category: {{ $product->category->name }}
+            </p>
+            <button class="btn orange btn-large">Buy</button>
+        </div>
+    </div>
+
+@endsection
