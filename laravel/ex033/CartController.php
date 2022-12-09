9d14173fe546f2392137008f267fe94737765e6f
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-12-10 08:16:21 +0900

    redirectwith

---
+++ laravel/ex033/CartController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Route;
+
+class CartController extends Controller
+{
+    public function cartList() {
+        $items = \Cart::getContent();
+        return view('site.cart', compact('items'));
+    }
+
+    public function addCart(Request $request) {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->qnt,
+            'attributes' => array(
+                'image' => $request->img
+            ),
+        ]);
+
+        return redirect()->route('site.cart')->with('success', 'The product was successfully added');
+    }
+}
