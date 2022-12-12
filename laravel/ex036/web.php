625de32281945924cf1bc72d7123cc1e3628554e
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-12-13 05:07:02 +0900

    clear cart function

---
+++ laravel/ex036/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SiteController;
+
+Route::resource('products', ProductController::class);
+
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
+Route::get('/product/{slug}', [SiteController::class, 'details'])->name('site.details');
+Route::get('/category/{id}', [SiteController::class, 'category'])->name('site.category');
+
+Route::get('/cart', [CartController::class, 'cartList'])->name('site.cart');
+Route::post('/cart', [CartController::class, 'addCart'])->name('site.addcart');
+Route::post('/remove', [CartController::class, 'removeCart'])->name('site.removecart');
+Route::post('/update', [CartController::class, 'updateCart'])->name('site.updatecart');
+Route::get('/clear', [CartController::class, 'clearCart'])->name('site.clearcart');
